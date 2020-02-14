89db3da7e03759a3e95af72b51956b8062a8c5eb
Chiến <caochientp1@gmail.com>
2020-02-14 11:25:21 +0700

    move cookie secret to environment variables

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config() // use module dotenv to use file .env. It stores variable which we add to env varialbes
 // Call library used in project.
 const express = require("express");
 const bodyParser = require("body-parser"); // To get data to req.body
 const cookieParser = require("cookie-parser");
-
+//console.log(process.env)
 var userRoute = require("./routes/user.route");
 var authRoute = require("./routes/auth.route");
 const { requireAuth } = require("./middlewares/auth.middleware");
@@ -15,7 +16,10 @@ app.set("views", "./view");
 // Set middlewares
 app.use(bodyParser.json()); // for paresing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser('sada'));
+// using the environment variable to stored.
+// purpose: do not want to store this variable in history commit
+// Thanks to modules dotenv, we can use 'process.env'. It used to access to obj has stored all env variables. 
+app.use(cookieParser(process.env.SESSION_SECRET)); 
 
 app.use(express.static("public" + __dirname));
 
